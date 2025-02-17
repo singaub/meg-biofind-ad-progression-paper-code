8135e8d796a7d18b32bdb776fc88ade9caffbbb1
dengemann <denis.engemann@gmail.com>
2025-02-17 12:16:53 +0100

    table

---
+++ table_S1.R
@@ -0,0 +1,123 @@
+# Code for Supplementary Table 1
+
+library(data.table)
+library(margins)
+library(lmtest)
+library(ggplot2)
+library(gridExtra)
+library(ggeffects)
+library(stargazer)
+library(coefplot)
+library(pROC)
+library(ROCR)
+library(dplyr)
+library(caret)
+
+df1=fread('aseg.vol.table')
+df1 <- df1 %>%
+  rename(participant_id = 'Measure:volume') %>%
+  rename(`RightAmygdala` = 'Right-Amygdala') %>%
+  rename(`LeftAmygdala` = 'Left-Amygdala') %>%
+  rename(`RightHippocampus` = 'Right-Hippocampus') %>%
+  rename(`WMHyperintensities` = 'WM-hypointensities') %>%
+  rename(`LeftHippocampus` = 'Left-Hippocampus') %>%
+  mutate(participant_id = gsub('sub-|_recon', '', participant_id))
+
+df1$ventricles <-rowSums(df1[,c('Left-Lateral-Ventricle',
+                                'Right-Lateral-Ventricle',
+                                'Left-Inf-Lat-Vent',
+                                'Right-Lateral-Ventricle'
+                                )])
+df1$Hippocampus <-rowMeans(df1[,c('RightHippocampus', 'LeftHippocampus')])
+df1$Amygdala <-rowMeans(df1[,c('RightAmygdala', 'LeftAmygdala')])
+
+df1$CortexVol <-rowMeans(df1[,c('lhCortexVol', 'rhCortexVol')])
+
+df1$TotalBrain <- rowSums(df1[,c('CerebralWhiteMatterVol',
+                                 'TotalGrayVol'
+                                  )])
+
+sdf1$CerebralWhiteMatterVol <- scale(log(df1[,c('CerebralWhiteMatterVol')]))
+
+df1$ratio_Hippocampus_TotalGrayVol <- scale(df1[,c('Hippocampus')]/df1[,c('TotalGrayVol')])
+df1$ratio_GreyMatterVolume_EstimatedTotalIntraCranialVol <- scale(df1[,c('TotalGrayVol')]/df1[,c('EstimatedTotalIntraCranialVol')])
+
+df1$WMHyperintensities <- scale(df1[,c('WMHyperintensities')])
+
+df=fread('stats_model_data.csv') # file containing cluster MEG PSD data head position aligned and MMSE adjusted
+df_converter=df[group=='patient']
+df_converter=df[!is.na(Converters)]
+
+df_converter$meg_average_cluster_adj <-rowMeans(df_converter[,c("megf16_post_adjust", "megf18_4_post_adjust", "megf22_6_post_adjust", "megf27_9_post_adjust", "megf34_3_post_adjust", "megf35_post_adjust", "megf36_8_post_adjust", "megf38_post_adjust")])
+
+merged_df <- merge(df1, df_converter, by = 'participant_id', all = FALSE)
+merged_df$participant_id <- NULL
+write.csv(merged_df, "merged_df.csv", row.names = FALSE)
+
+#MOD1
+mod1 <- glm(Converters ~ scale(age) + scale(Edu_years) + scale(MMSE), data = merged_df, family = binomial)
+summary(mod1)
+summary(margins(mod1))
+summary_marg_mod1 <-summary(margins(mod1))
+write.csv(summary_marg_mod1, "mod1_age_edu_mmse.csv", row.names = FALSE)
+
+#MOD2
+mod2 <- glm(Converters ~ scale(age) + scale(Edu_years) + scale(MMSE) + ratio_Hippocampus_TotalGrayVol, data = merged_df, family = binomial)
+summary(mod2)
+summary(margins(mod2))
+summary_marg_mod2 <-summary(margins(mod2))
+write.csv(summary_marg_mod2, "mod2_age_edu_mmse_Hipp_ratio.csv", row.names = FALSE)
+
+# MOD3
+mod3 <- glm(Converters ~ scale(age) + scale(Edu_years) + scale(meg_average_cluster_adj), data = merged_df, family = binomial(link="logit"))
+summary(mod3)
+summary(margins(mod3))
+summary_marg_mod3 <-summary(margins(mod3))
+write.csv(summary_marg_mod3, "mod3_age_edu_mmse_MEG.csv", row.names = FALSE)
+
+# MOD4
+mod4 <- glm(Converters ~ scale(age) + scale(Edu_years) + scale(MMSE) + scale(meg_average_cluster_adj) + ratio_Hippocampus_TotalGrayVol, data = merged_df, family = binomial(link="logit"))
+summary(mod4)
+summary(margins(mod4))
+summary_marg_mod4 <-summary(margins(mod4))
+write.csv(summary_marg_mod4, "mod4_age_edu_mmse_MEG_Hipp_ratio.csv", row.names = FALSE)
+
+# compare models
+AIC(mod1, mod2, mod3, mod4)
+
+# PLOT AUC
+# ROC data
+roc_data_mod1 <- roc(merged_df$Converters, predict(mod1, type = "response"))
+roc_coordinates <- coords(roc_data_mod1, "all")
+write.csv(roc_coordinates, "roc_data_mod1.csv", row.names = FALSE)
+
+roc_data_mod2 <- roc(merged_df$Converters, predict(mod2, type = "response"))
+roc_coordinates <- coords(roc_data_mod2, "all")
+write.csv(roc_coordinates, "roc_data_mod2.csv", row.names = FALSE)
+
+roc_data_mod3 <- roc(merged_df$Converters, predict(mod3, type = "response"))
+roc_coordinates <- coords(roc_data_mod3, "all")
+write.csv(roc_coordinates, "roc_data_mod3.csv", row.names = FALSE)
+
+roc_data_mod4 <- roc(merged_df$Converters, predict(mod4, type = "response"))
+roc_coordinates <- coords(roc_data_mod4, "all")
+write.csv(roc_coordinates, "roc_data_mod4.csv", row.names = FALSE)
+
+# Compute sensitivity, specificity, AUC, Accuracy
+predicted_labels <- ifelse(predict(mod1, type = "response") > 0.5, 1, 0)
+actual_labels <- merged_df$Converters
+
+true_positives <- sum(predicted_labels == 1 & actual_labels == 1)
+false_negatives <- sum(predicted_labels == 0 & actual_labels == 1)
+true_negatives <- sum(predicted_labels == 0 & actual_labels == 0)
+false_positives <- sum(predicted_labels == 1 & actual_labels == 0)
+
+sensitivity_mod1 <- true_positives / (true_positives + false_negatives)
+specificity_mod1 <- true_negatives / (true_negatives + false_positives)
+accuracy_mod1 <- (true_positives + true_negatives) / (true_positives + true_negatives + false_positives + false_negatives)
+auc_mod1 <- auc(roc_data_mod1)
+
+print(accuracy_mod1)
+print(auc_mod1)
+print(sensitivity_mod1)
+print(specificity_mod1)
